e86837ca50c2a5a67dd51083cdd620a67084059a
Jérôme Petazzoni <jerome.petazzoni@gmail.com>
2025-04-03 14:26:01 +0200

    Generate a recipe with a hard-coded prompt

--- src/main/java/training/container/cookbook/RecipeGenerator.java
+++ src/main/java/training/container/cookbook/RecipeGenerator.java
@@ -1,14 +1,21 @@
 package training.container.cookbook;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecipeGenerator {
 
+    private final ChatClient chatClient;
+
+    RecipeGenerator(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
     @GetMapping("/recipe")
-    public String generateRecipe(String instructions) {
-        return "hello";
+    public String generateRecipe() {
+        return chatClient.prompt("Generate a cooking recipe for a delicious main dish.").call().content();
     }
 
 }
